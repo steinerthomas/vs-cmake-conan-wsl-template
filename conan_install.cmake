4b370edb08d948d18de76ee3826210c39f133534
Thomas Steiner <thomas.steiner@engel.at>
2025-02-05 15:33:34 +0100

    remove commented line

--- conan_install.cmake
+++ conan_install.cmake
@@ -6,5 +6,3 @@ execute_process(
 	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 	RESULT_VARIABLE CONAN_RESULT
 )
-
-#set(CMAKE_TOOLCHAIN_FILE "${CMAKE_BINARY_DIR}/build/${CMAKE_BUILD_TYPE}/generators/conan_toolchain.cmake")
